769656c41df0e663c7bb46abd2d8a2fd000c0920
arrooxa <cambui01_@hotmail.com>
2024-01-15 06:29:50 -0300

    fix: remove env variables to help in applications bootstrap

--- web/app/services/getTableInfo.ts
+++ web/app/services/getTableInfo.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { RecordsResponse } from "@app/interfaces/records";
-import { publicRuntimeConfig } from "../../next.config.js";
 
 export async function getTableInfo(
   take: number,
   page: number
 ): Promise<RecordsResponse> {
-  const fetchData = await axios.get(`localhost:3001/records`, {
+  const fetchData = await axios.get(`http://localhost:3001/records`, {
     params: {
       take,
       page,
